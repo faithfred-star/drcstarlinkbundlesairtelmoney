380c96df9f44093be3d60859cc2e5e06b1a42ecc
faithfred-star <faithfred721@gmail.com>
2026-06-01 22:12:49 +0300

    Automate superuser creation in build script

--- build.sh
+++ build.sh
@@ -10,3 +10,4 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput --clear
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'faithfred721@gmail.com', '1234!')"
