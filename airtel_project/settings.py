3e65406471be290516fbcad4321936c3a1fe3108
faithfred-star <faithfred721@gmail.com>
2026-06-07 23:21:09 +0300

    Fix Bad Request 400 by adding Render to ALLOWED_HOSTS

--- airtel_project/settings.py
+++ airtel_project/settings.py
@@ -22,7 +22,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'starlinkairtelmoneydrcbundles.onrender.com',
+    'drcstarlinkbundlesairtelmoney.onrender.com',
 ]
 
 # Application definition
